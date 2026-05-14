38a341cb6dd2a4bc7021711f0a3385fe813d0cd5
zaid-6969 <bawasamohammedzaid@gmail.com>
2026-05-14 11:46:36 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.json());
 
 const allowedOrigins = [
+  "https://portfolio-backend-livid-eight.vercel.app",
   "http://localhost:5173",
   "http://localhost:3000",
 ];
